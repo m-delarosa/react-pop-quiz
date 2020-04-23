3e04374da7941aa27ef9bbbc8775c1801e22d2fe
Michael De La Rosa <56710845+m-delarosa@users.noreply.github.com>
2020-04-23 10:52:17 -0600

    Working version. Submitted after deadline.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import './App.css'
 import Mike from "./Mike"
+import Jon from "./Jon"
+import Tyler from "./Tyler"
 
 class App extends Component {
 
@@ -19,6 +21,13 @@ class App extends Component {
         location: "Tampa, FL",
         profile_link: 'www.google.com',
         photo: "https://cms.qz.com/wp-content/uploads/2016/10/gorilla.jpg?quality=75&strip=all&w=1600&h=900&crop=1"
+      },
+      {
+        id: 3,
+        name: "Tyler Funk",
+        location: "Kansas City, MO",
+        profile_link: 'www.google.com',
+        photo: "https://www.rainforest-alliance.org/sites/default/files/styles/750w_585h/public/2016-09/capuchin-monkey-baby.jpg?itok=4uOxFicS"
       }
     ]
   }
@@ -26,19 +35,26 @@ class App extends Component {
   render() {
     return (
       <div className="App" >
-        <h1> Flatiron SE Program Students </h1>
+        <h1> Flatiron School Software Engineering Students </h1>
+        <h4> Someone Please Hire Them </h4>
         <section class="card-container">
           <Mike
-            name={this.state.students.0.name}
-            location={this.state.students.0.location}
-            link={this.state.students.0.profile_link}
-            photo={this.state.students.0.photo}
+            name={this.state.students[0].name}
+            location={this.state.students[0].location}
+            link={this.state.students[0].profile_link}
+            photo={this.state.students[0].photo}
           />
           <Jon
-            name={this.state.students.1.name}
-            location={this.state.students.1.location}
-            link={this.state.students.1.profile_link}
-            photo={this.state.students.1.photo}
+            name={this.state.students[1].name}
+            location={this.state.students[1].location}
+            link={this.state.students[1].profile_link}
+            photo={this.state.students[1].photo}
+          />
+          <Tyler
+            name={this.state.students[2].name}
+            location={this.state.students[2].location}
+            link={this.state.students[2].profile_link}
+            photo={this.state.students[2].photo}
           />
         </section>
       </div>
--- src/Mike.js
+++ src/Mike.js
@@ -1,13 +1,14 @@
 import React from "react"
 
 export default function card(props) {
-    console.log(props)
 
     return (
-        <img src='{props.photo}'>
+        <section>
+            <img src={props.photo}></img>
             <h2>{props.name}</h2>
             <p>{props.location}</p>
             <a href={props.link}>View Profile</a>
+        </section>
     )
 
 }
